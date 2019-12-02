5452861a9bb02700d5cb3eb4290245869217590e
Romain GRONDIN <romain.grondin@centrale-marseille.fr>
2019-12-02 16:23:51 +0100

    Refactored socket io callbacks

---
+++ routes/socket.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-param-reassign */
+const { makeId } = require('./utils');
+
+// eslint-disable-next-line func-names
+module.exports = function (app, io) {
+  // Helper functions
+  function findKey(id) {
+    for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
+      const c = app.locals.clients[i];
+
+      if (c.clientId === id) {
+        return c.clientKey;
+      }
+    }
+    return null;
+  }
+
+  function deleteId(clientKey) { // sera utile pour déconnecter les users
+    for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
+      const c = app.locals.clients[i];
+
+      if (c.clientKey === clientKey) {
+        app.locals.clients.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  // Client callbacks
+  io.on('connection', (socket) => {
+    console.log(`Socket ${socket.id} connected`);
+    for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
+      const c = app.locals.clients[i];
+      console.log(c.clientKey);
+    }
+    // console.log(app.locals.clients);
+
+
+    socket.on('storeClientInfo', (data) => {
+      for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
+        const c = app.locals.clients[i];
+        // console.log(data.clientKey);
+        if (c.clientKey === data.clientKey) {
+          app.locals.clients[i].clientId = socket.id;
+          // console.log(`${app.locals.clients[i].clientId} ${app.locals.clients[i].clientKey}`);
+          console.log(app.locals.clients);
+          break;
+        }
+      }
+    });
+
+    socket.on('display', () => {
+      app.locals.displayId = socket.id;
+      io.to(app.locals.displayId).emit('client_list', app.locals.clients);
+      console.log(`Borne id: ${app.locals.displayId}`);
+    });
+
+    socket.on('set_color', (data) => {
+      for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
+        if (app.locals.clients[i].clientKey === data.client) {
+          io.to(app.locals.clients[i].clientId).emit('set_color', data.color);
+        }
+      }
+      console.log(data);
+    });
+
+    socket.on('cursor', (data) => {
+      // console.log('Mobile id:' + cursorId);
+      io.to(app.locals.displayId).emit('displayCursor', data.clientKey);
+    });
+
+    socket.on('move', (data) => {
+      io.to(app.locals.displayId).emit('data', data);
+    });
+
+    socket.on('click', (data) => {
+      io.to(app.locals.displayId).emit('remote_click', data);
+    });
+
+    socket.on('start_posting', (data) => {
+      io.to(data).emit('start_posting');
+    });
+
+    socket.on('posting', (content) => {
+      io.to(app.locals.displayId).emit('posting', content);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+      const key = findKey(socket.id);
+      deleteId(key);
+      io.to(app.locals.displayId).emit('disconnect_user', key);
+      if (app.locals.clients.length === 3
+        && app.locals.clients[app.locals.clients.length - 1].clientId !== null) {
+        const clientKey = makeId(8);
+        app.locals.clients.push({ clientKey, clientId: null });
+        io.to(app.locals.displayId).emit('reload_qr');
+      }
+      console.log(app.locals.clients);
+    });
+  });
+};
--- server.js
+++ server.js
@@ -15,37 +15,14 @@ app.set('io', io);
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 
-
-function findKey(id) {
-  for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
-    const c = app.locals.clients[i];
-
-    if (c.clientId === id) {
-      return c.clientKey;
-    }
-  }
-  return null;
-}
-
 const displayRoutes = require('./routes/display');
 const mobileRoutes = require('./routes/mobile');
+require('./routes/socket')(app, io);
 
 app.use(displayRoutes);
 app.use(mobileRoutes);
 
-
-function deleteId(clientKey) { // sera utile pour déconnecter les users
-  for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
-    const c = app.locals.clients[i];
-
-    if (c.clientKey === clientKey) {
-      app.locals.clients.splice(i, 1);
-      break;
-    }
-  }
-}
-
-let clientKey = makeId(8); // last client key
+const clientKey = makeId(8); // last client key
 app.locals.clients.push({ clientKey, clientId: null });
 
 app.get('/ping', (req, res) => res.send('pong'));
@@ -60,79 +37,6 @@ app.get('/', (req, res) => {
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-io.on('connection', (socket) => {
-  console.log(`Socket ${socket.id} connected`);
-  for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
-    const c = app.locals.clients[i];
-    console.log(c.clientKey);
-  }
-  // console.log(app.locals.clients);
-
-
-  socket.on('storeClientInfo', (data) => {
-    for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
-      const c = app.locals.clients[i];
-      // console.log(data.clientKey);
-      if (c.clientKey === data.clientKey) {
-        app.locals.clients[i].clientId = socket.id;
-        // console.log(`${app.locals.clients[i].clientId} ${app.locals.clients[i].clientKey}`);
-        console.log(app.locals.clients);
-        break;
-      }
-    }
-  });
-
-  socket.on('display', () => {
-    app.locals.displayId = socket.id;
-    io.to(app.locals.displayId).emit('client_list', app.locals.clients);
-    console.log(`Borne id: ${app.locals.displayId}`);
-  });
-
-  socket.on('set_color', (data) => {
-    for (let i = 0, len = app.locals.clients.length; i < len; i += 1) {
-      if (app.locals.clients[i].clientKey === data.client){
-        io.to(app.locals.clients[i].clientId).emit('set_color', data.color);
-      }
-    }
-    console.log(data);
-  });
-
-  socket.on('cursor', (data) => {
-    // console.log('Mobile id:' + cursorId);
-    io.to(app.locals.displayId).emit('displayCursor', data.clientKey);
-  });
-
-  socket.on('move', (data) => {
-    io.to(app.locals.displayId).emit('data', data);
-  });
-
-  socket.on('click', (data) => {
-    io.to(app.locals.displayId).emit('remote_click', data);
-  });
-
-  socket.on('start_posting', (data) => {
-    io.to(data).emit('start_posting');
-  });
-
-  socket.on('posting', (content) => {
-    io.to(app.locals.displayId).emit('posting', content);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-    const key = findKey(socket.id);
-    deleteId(key);
-    io.to(app.locals.displayId).emit('disconnect_user', key);
-    if (app.locals.clients.length === 3
-      && app.locals.clients[app.locals.clients.length - 1].clientId !== null) {
-      clientKey = makeId(8);
-      app.locals.clients.push({ clientKey, clientId: null });
-      io.to(app.locals.displayId).emit('reload_qr');
-    }
-    console.log(app.locals.clients);
-  });
 });
 
 console.log(process.env[process.env.PORT]);
