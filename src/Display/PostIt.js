a4cff3cdb7a1d52bc84816084ef27af855bbc404
Antoine Mirande <antoine.mirande@outlook.fr>
2020-02-12 12:32:53 +0100

    adapt text size to text length

--- src/Display/PostIt.js
+++ src/Display/PostIt.js
@@ -4,6 +4,15 @@ function PostIt(props) {
   const {
     key, id, text, x, y, z,
   } = props;
+  const textLength = text.length;
+  let fontSize = '25pt';
+  if (textLength > 16 && textLength <= 20) {
+    fontSize = '20pt';
+  } else if (textLength > 20 && textLength <= 58) {
+    fontSize = '15pt';
+  } else if (textLength > 58) {
+    fontSize = '10pt';
+  }
   return (
     <div
       className="postit"
@@ -13,6 +22,7 @@ function PostIt(props) {
         left: `${x}px`,
         top: `${y}px`,
         zIndex: z,
+        fontSize,
       }}
     >
       {text}
