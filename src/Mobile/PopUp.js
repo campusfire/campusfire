05c101a364efd36814c455f13cefd4953ee6ead2
fanismichalakis <fanis.michalakis@gmail.com>
2021-01-17 09:55:50 +0100

    add scroll inside popup

--- src/Mobile/PopUp.js
+++ src/Mobile/PopUp.js
@@ -7,12 +7,14 @@ const Popup = function ({ text, closePopup }) {
     <div className="popup">
       <div className="popup_inner">
         <button className="closeButton" type="button" onClick={closePopup}>X</button>
-        <h1>{text}</h1>
-        <p>Utilisez l'écran de votre smartphone pour déplacer le curseur sur le grand écran.</p>
-        <p>Pour ajouter un élément, restez appuyé jusqu'à ce qu'une roue de sélection apparaisse sur le grand écran,
-           et choisissez le type de contenu que vous souhaitez ajouter.</p>
-        <p>Vous pouvez déplacer un élement en le sélectionnant par un appui long, puis en vous déplaçant en restant appuyé</p>
-        <p>Un tutoriel vidéo est également disponible <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">ici</a>.</p>
+        <div className="popup_scrollable">
+          <h1>{text}</h1>
+          <p>Utilisez l'écran de votre smartphone pour déplacer le curseur sur le grand écran.</p>
+          <p>Pour ajouter un élément, restez appuyé jusqu'à ce qu'une roue de sélection apparaisse sur le grand écran,
+             et choisissez le type de contenu que vous souhaitez ajouter.</p>
+          <p>Vous pouvez déplacer un élement en le sélectionnant par un appui long, puis en vous déplaçant en restant appuyé</p>
+          <p>Un tutoriel vidéo est également disponible <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">ici</a>.</p>
+        </div>
       </div>
     </div>
   );
