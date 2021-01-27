c8990d92cbb54d29d79f96324510a95c5677815c
Maxime Vivier <maximevivier92@gmail.com>
2021-01-27 15:18:40 +0100

    change the launch server script

--- launch_server.sh
+++ launch_server.sh
@@ -1,4 +1,1 @@
-cd node;
-npm install;
-echo "ENV='PROD'" >> .env;
-/usr/bin/screen -d -m -S node node server.js;
+cd node;npm install;echo "ENV='PROD'" >> .env;/usr/bin/screen -d -m -S node node server.js;
