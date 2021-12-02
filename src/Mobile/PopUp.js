5ddc5f624476deec1dd22b68b1b44c61aedf9968
SimonRosee <simon.rose3113@gmail.com>
2021-12-02 10:52:15 +0100

    Change mobile popup help for concours

--- src/Mobile/PopUp.js
+++ src/Mobile/PopUp.js
@@ -10,8 +10,10 @@ const Popup = function ({ text, closePopup }) {
         <div className="popup_scrollable">
           <h1>{text}</h1>
           <p>Utilisez l'écran de votre smartphone pour déplacer le curseur sur le grand écran.</p>
-          <p>Pour ajouter un élément, restez appuyé jusqu'à ce qu'une roue de sélection apparaisse sur le grand écran,
-             et choisissez le type de contenu que vous souhaitez ajouter.</p>
+          <p>Pour ajouter un élément, restez appuyé jusqu'à ce qu'un icône apparaisse sur le grand écran puis relachez.
+            Vous pouvez alors choisir une image à poster depuis votre téléphone.
+          </p>
+          <p>Vous pouvez liker/disliker les images des autres utilisateurs afin de modifier leur durée de vie à l'écran.</p>
           <p>Vous pouvez déplacer un élement en le sélectionnant par un appui long, puis en vous déplaçant en restant appuyé</p>
           <p>Un tutoriel vidéo est également disponible <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">ici</a>.</p>
         </div>
